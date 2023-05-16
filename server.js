6362554f7e9d716f30b76153774bffe1107b4b78
Linnie <linnie@kiwico.com>
2023-05-16 13:33:43 -0700

    fix: Resolve es-lint issues and bugs in server.js

--- server.js
+++ server.js
@@ -3,22 +3,23 @@ const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((req, res) => {
-  let filePath = req.url === '/' ? '/index.php' : req.url;
-  filePath = path.join(__dirname, filePath);
+    let filePath = req.url === '/' ? '/index.html' : req.url;
+    filePath = path.join(__dirname, filePath);
 
-  fs.readFile(filePath, 'utf-8', (err, content) => {
-    if (err) {
-      res.writeHead(404);
-      res.end('File not found!');
-      return;
-    }
+    fs.readFile(filePath, 'utf-8', (err, content) => {
+        if (err) {
+            res.writeHead(404);
+            res.end('File not found!');
+            return;
+        }
 
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(content);
-  });
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content);
+    });
 });
 
-const port = 3000;
+const port = 8080;
 server.listen(port, () => {
-  console.log(`Server running on http://localhost:8080`);
+    // eslint-disable-next-line no-console
+    console.log('Server running on http://localhost:8080');
 });
