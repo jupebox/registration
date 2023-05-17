44b810ad9fb8fe4b02cb91368a24ec5059594950
Linnie <linnie@kiwico.com>
2023-05-16 19:49:04 -0700

    feat: First pass at registration form requirements

--- script.js
+++ script.js
@@ -1,92 +1,163 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const form = document.getElementById('#registration-form');
-  const nameInput = document.getElementById('#name');
-  const emailInput = document.getElementById('#email');
-  const passwordInput = document.getElementById('#password');
-  const confirmPasswordInput = document.getElementById('.confirm-password');
+    const form = document.getElementById('registration-form');
+    const nameInput = document.getElementById('name');
+    const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('password');
+    const confirmPasswordInput = document.getElementById('confirm-password');
+    const errorDialog = document.getElementById('error-dialog');
+    const successDialog = document.getElementById('success-dialog');
+    const dialogOverlay = document.querySelector('.dialog__overlay');
 
-  form.addEventListener('submit', (e) => {
-    clearErrorMessages();
+    // this seems a little restrictive to me considering foreign languages, special characters, hyphenated names, etc, but don't want to mess with the requirements
+    const validateName = name => /^[a-zA-Z]+$/.test(name);
 
-    const name = nameInput.value.trim();
-    const email = emailInput.value.trim();
-    const password = passwordInput.value.trim();
-    const confirmPassword = confirmPasswordInput.value.trim();
+    // Basic email validation, can be improved
+    // emails come in so many formats though that it's usually better to just let people do what they want, imo /shrug
+    const validateEmail = email => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 
-    if (!validateName(name)) {
-      showError(nameInput, 'Please enter a valid name.');
-      return;
-    }
+    // Password must contain at least 8 characters, one uppercase, one lowercase, one digit
+    const validatePassword = password => /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/.test(password);
 
-    if (!validateEmail(email)) {
-      showError(emailInput, 'Please enter a valid email address.');
-      return;
-    }
+    const validateConfirmPassword = (confirmPassword) => {
+        const password = passwordInput.value.trim();
+        return confirmPassword === password;
+    };
 
-    if (!validatePassword(password)) {
-      showError(passwordInput, 'Please enter a valid password (at least 8 characters, one uppercase, one lowercase, one digit).');
-      return;
-    }
+    const validations = [
+        {
+            input: nameInput,
+            message: 'Please enter a valid name.',
+            validate: validateName
+        },
+        {
+            input: emailInput,
+            message: 'Please enter a valid email address.',
+            validate: validateEmail
+        },
+        {
+            input: passwordInput,
+            message: 'Please enter a valid password (at least 8 characters, one uppercase, one lowercase, one digit).',
+            validate: validatePassword
+        },
+        {
+            input: confirmPasswordInput,
+            message: 'Passwords do not match.',
+            validate: validateConfirmPassword
+        }
+    ];
 
-    if (password !== confirmPassword) {
-      showError(confirmPasswordInput, 'Passwords do not match.');
-      return;
-    }
+    validations.forEach((validation) => {
+        const { input, message, validate } = validation;
+        input.addEventListener('blur', (e) => {
+            input.classList.remove('form__input--valid');
+            const { target } = e;
+            const { value } = target;
+            const trimmedValue = value.trim();
+            if (trimmedValue) {
+                if (validate(trimmedValue) && !input.classList.contains('form__input--valid')) {
+                    input.classList.add('form__input--valid');
+                } else {
+                    showError(input, message);
+                }
+            }
+        });
 
-    // Form validation successful, do further processing or submit the form
-    console.log('Form submitted successfully!');
-    fetchpost(e);
-  });
+        input.addEventListener('keyup', (e) => {
+            clearErrorMessage(input);
+            const { target } = e;
+            const { value } = target;
+            const trimmedValue = value.trim();
+            if (trimmedValue) {
+                if (validate(trimmedValue)) {
+                    input.classList.add('form__input--valid');
+                }
+            }
+        });
+    });
 
-    function fetchpost(e) {
+    document.querySelectorAll('.dialog__button').forEach((element) => {
+        element.addEventListener('click', (e) => {
+            e.target.parentElement.classList.remove('dialog--visible');
+            dialogOverlay.classList.remove('dialog__overlay--visible');
+        });
+    });
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        if (form.classList.contains('form--processing')) {
+            return;
+        }
+        clearErrorMessages();
+        const errors = [];
+        validations.forEach((validation) => {
+            const { input, validate } = validation;
+            const trimmedValue = input.value.trim();
+            if (!trimmedValue || !validate(trimmedValue)) {
+                errors.push(validation);
+            }
+        });
+
+        if (errors.length > 0) {
+            errors.forEach((error) => {
+                const { input, message } = error;
+                showError(input, message);
+            });
+            return;
+        }
+
+        // Form validation successful, do further processing or submit the form
+        submitForm(e);
+    });
+
+    const submitForm = (e) => {
+        form.classList.add('form--processing');
         // (A) GET FORM DATA
-        var data = new FormData(e.currentTarget);
-
+        const data = new FormData(e.currentTarget);
         // (B) FETCH
-        fetch(e.currentTarget.action, { method: "post", body: data })
+        fetch(e.currentTarget.action, { method: 'post', body: data })
             .then(res => res.text())
-            .then(txt => {
-                // do something when server responds
-                console.log(txt);
+            .then(() => {
+                form.classList.remove('form--processing');
+                form.classList.add('form--submitted');
+                dialogOverlay.classList.add('dialog__overlay--visible');
+                successDialog.classList.add('dialog--visible');
             })
-            .catch(err => console.log(err));
+            .catch((err) => {
+                form.classList.remove('form--processing');
+                errorDialog.querySelector('.dialog__text').innerText = err.message;
+                dialogOverlay.classList.add('dialog__overlay--visible');
+                errorDialog.classList.add('dialog--visible');
+            });
 
         // (C) PREVENT HTML FORM SUBMIT
+        // not certain this does anything that e.preventDefault isn't already doing
         return false;
-    }
+    };
 
-  function validateName(name) {
-    return /^[a-zA-Z]+$/.test(name);
-  }
+    const showError = (input, message) => {
+        if (input.classList.contains('form__input--error')) {
+            return;
+        }
+        const formGroup = input.parentElement;
+        const errorMessage = document.createElement('label');
+        errorMessage.className = 'form__message form__message--error';
+        errorMessage.innerText = message;
+        errorMessage.setAttribute('for', input.id);
+        formGroup.appendChild(errorMessage);
+        input.classList.add('form__input--error');
+    };
 
-  function validateEmail(email) {
-    // Basic email validation, can be improved
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-  }
+    const clearErrorMessage = (input) => {
+        if (input.classList.contains('form__input--error')) {
+            input.classList.remove('form__input--error');
+            input.parentElement.removeChild(document.querySelector('.form__message--error'));
+        }
+    };
 
-  function validatePassword(password) {
-    // Password must contain at least 8 characters, one uppercase, one lowercase, one digit
-    return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/.test(password);
-  }
-
-  function showError(input, message) {
-    const formGroup = input.parentElement;
-    const errorMessage = document.createElement('div');
-    errorMessage.className = 'error-message';
-    errorMessage.innerText = message;
-    formGroup.appendChild(errorMessage);
-    input.classList.add('error');
-  }
-
-  function clearErrorMessages() {
-    const errorMessages = document.querySelectorAll('.error-message');
-    errorMessages.forEach((errorMessage) => {
-      errorMessage.parentElement.removeChild(errorMessage);
-    });
-
-    const errorInputs = document.querySelectorAll('.error');
-    errorInputs.forEach((errorInput) => {
-      errorInput.classList.remove('error');
-    });
-  }
+    const clearErrorMessages = () => {
+        validations.forEach((validation) => {
+            const { input } = validation;
+            clearErrorMessage(input);
+        });
+    };
 });
